0e15918f1d196f57bef36c9ef77d6dcfdf507356
atharva vadodkar <158564201+atharva-1304@users.noreply.github.com>
2024-03-08 19:05:13 +0530

    add

---
+++ Number_game.java
@@ -0,0 +1,25 @@
+package number_Game;
+import java.util.Scanner;
+
+public class Number_game {
+
+  public static void main(String[] args) {
+    Scanner Sc = new Scanner(System.in);
+    int MyNum = (int) (Math.random() * 100);
+    int UseerNumber;
+    do {
+      System.out.println("enter a number:");
+      UseerNumber = Sc.nextInt();
+      if (MyNum == UseerNumber) {
+        System.out.println("you win");
+        break;
+      } else if (UseerNumber > MyNum) {
+        System.out.println("large number");
+      } else {
+        System.out.println("number is smalll:");
+      }
+    } while (UseerNumber >= 0);
+    System.out.println("my number:");
+    System.out.println(MyNum);
+  }
+}
